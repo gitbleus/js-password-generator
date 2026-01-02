5db74036016d8ec84dc154203710c1c8ab239456
bleus <bleus007@protonmail.com>
2026-01-02 13:10:28 -0300

    Initial commit: Password Generator

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './assets/css/style.css';
+import form from './modules/form'
+
+form()
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /nodes_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader', 'postcss-loader']
+    }]
+  },
+  devtool: 'source-map'
+};
